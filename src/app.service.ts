2424af7896e7aba47a1ceb37fb9f4a8c8226321b
Edwin Ramirez <tavo198718@gmail.com>
2022-06-06 19:26:47 -0500

    Update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const fullName = 'Hello, NestJS';
+    const fullName = 'Hello, NestJSsss';
     return fullName;
   }
 
